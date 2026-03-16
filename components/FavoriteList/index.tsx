f8ab61fc413dfdda7eb3f14b57604aa026f1d1ee
kurushiba <156669290+kurushiba@users.noreply.github.com>
2026-03-16 17:48:07 +0900

    Update index.tsx

--- components/FavoriteList/index.tsx
+++ components/FavoriteList/index.tsx
@@ -2,10 +2,6 @@ import './index.css';
 import SpotCard from '../SpotCard';
 
 export default function FavoriteList() {
-  /* ローディング中のUI（コメントインで確認）
-  return <div className='favorite-list-loading'>読み込み中...</div>;
-  */
-
   /* 空状態のUI（コメントインで確認）
   return <div className='favorite-list-empty'>お気に入りがありません</div>;
   */
