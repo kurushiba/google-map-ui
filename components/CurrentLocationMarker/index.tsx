9d385fce630dada18d09e0c9f1ab2dc47648e896
kurushiba <156669290+kurushiba@users.noreply.github.com>
2026-03-17 10:17:23 +0900

    Update index.tsx

--- components/CurrentLocationMarker/index.tsx
+++ components/CurrentLocationMarker/index.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 // マップコンポーネントのため、react-leaflet / leaflet なしでは表示されません
+// const icon = '<div class="current-location-dot"></div><div class="current-location-pulse"></div>';
 
 export default function CurrentLocationMarker() {
   return null;
